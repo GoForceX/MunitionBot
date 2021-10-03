0722c746030c523f910fb787a45ac43cb0fbf674
GoForceX <master@goforcex.top>
2021-10-03 19:34:00 +0800

    chore: Reformat code

--- bfbot/plugins/query_server_info/__init__.py
+++ bfbot/plugins/query_server_info/__init__.py
@@ -9,7 +9,8 @@ from nonebot.adapters import Bot, Event
 import httpx
 from nonebot.log import logger
 
-query = on_command("server") 
+query = on_command("server")
+
 
 @query.handle()
 async def handle_message(bot: Bot, event: Event, state: T_State):
@@ -21,11 +22,15 @@ async def handle_message(bot: Bot, event: Event, state: T_State):
         await query.finish("命令格式突然不对劲起来了（笑\n格式是#server <bf1/bf4/bfv> <name>哦")
     if args[0] == "bf1" or args[0] == "bf4" or args[0] == "bfv":
         if args[0] and args[1]:
-            logger.debug("Requesting: https://api.gametools.network/" + args[0] + "/servers/?name=" + args[1] + "&lang=" + ("zh-cn" if args[0] == 'bfv' else "zh-tw") + "&region=all&platform=pc&limit=8")
+            logger.debug(
+                "Requesting: https://api.gametools.network/" + args[0] + "/servers/?name=" + args[1] + "&lang=" + (
+                    "zh-cn" if args[0] == 'bfv' else "zh-tw") + "&region=all&platform=pc&limit=8")
             async with httpx.AsyncClient() as client:
                 try:
                     await query.send("稍等下，正在获取您请求的信息……")
-                    resp = await client.get("https://api.gametools.network/" + args[0] + "/servers/?name=" + args[1] + "&lang=" + ("zh-cn" if args[0] == 'bfv' else "zh-tw") + "&region=all&platform=pc&limit=8", timeout=10.0)
+                    resp = await client.get(
+                        "https://api.gametools.network/" + args[0] + "/servers/?name=" + args[1] + "&lang=" + (
+                            "zh-cn" if args[0] == 'bfv' else "zh-tw") + "&region=all&platform=pc&limit=8", timeout=10.0)
                 except httpx.ReadTimeout:
                     await query.finish("太对不起了，请求超时了（悲")
                 result = resp.json()["servers"]
@@ -34,30 +39,36 @@ async def handle_message(bot: Bot, event: Event, state: T_State):
                     await query.finish("欸，找不到此名称对应的服务器")
                 image = Image.open(os.path.join(os.path.abspath('.'), 'static', f'{args[0]}-top8.png'))
                 drawer = ImageDraw.Draw(image)
-                font = ImageFont.truetype(os.path.join(os.path.abspath('.'), 'static', 'HarmonyOS_Sans_SC_Regular.ttf'), 20)
-                for i in range(4): 
+                font = ImageFont.truetype(
+                    os.path.join(
+                        os.path.abspath('.'),
+                        'static',
+                        'HarmonyOS_Sans_SC_Regular.ttf'
+                    ), 20)
+                for i in range(4):
                     for j in range(2):
-                        if 2*i+j >= len(result):
+                        if 2 * i + j >= len(result):
                             break
-                        drawer.text((83 + 416 * j, 40 + 163 * i),  "服务器名：" +
-                            str(result[2*i+j]["prefix"][:22] + ('...' if len(result[2*i+j]["prefix"]) > 22 else '')) +
-                            "\n状态：" +
-                            str(result[2*i+j]["serverInfo"]) +
-                            " [" +
-                            str(result[2*i+j]["inQue"]) +
-                            "]" +
-                            ("\n模式：" +
-                            str(result[2*i+j]["mode"])).ljust(10) +
-                            ("地图：" +
-                            str(result[2*i+j]["currentMap"])).center(14) +
-                            ("\n区服：" +
-                            str(result[2*i+j]["region"])).ljust(14) +
-                            ("平台：" +
-                            str(result[2*i+j]["platform"])).center(14), font=font, fill="#e0e0e0")
-                
+                        drawer.text((83 + 416 * j, 40 + 163 * i), "服务器名：" +
+                                    str(result[2 * i + j]["prefix"][:22] + (
+                                        '...' if len(result[2 * i + j]["prefix"]) > 22 else '')) +
+                                    "\n状态：" +
+                                    str(result[2 * i + j]["serverInfo"]) +
+                                    " [" +
+                                    str(result[2 * i + j]["inQue"]) +
+                                    "]" +
+                                    ("\n模式：" +
+                                     str(result[2 * i + j]["mode"])).ljust(10) +
+                                    ("地图：" +
+                                     str(result[2 * i + j]["currentMap"])).center(14) +
+                                    ("\n区服：" +
+                                     str(result[2 * i + j]["region"])).ljust(14) +
+                                    ("平台：" +
+                                     str(result[2 * i + j]["platform"])).center(14), font=font, fill="#e0e0e0")
+
                 img_io = BytesIO()
                 image.save(img_io, format="PNG")
-                
+
                 await query.finish(
                     Message(
                         (MessageSegment.at(sender_id) if event.get_session_id().startswith('group') else '') +
