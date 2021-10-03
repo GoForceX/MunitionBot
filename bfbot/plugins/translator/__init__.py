453fff5e2df706b0f54793870c4a58f9af06b4b1
GoForceX <master@goforcex.top>
2021-10-03 19:07:40 +0800

    fix(translator): Change key to lower case.

--- bfbot/plugins/translator/__init__.py
+++ bfbot/plugins/translator/__init__.py
@@ -8,8 +8,9 @@ from nonebot import on_command
 from nonebot.adapters import Bot, Event
 from nonebot.adapters.cqhttp import Message, MessageSegment
 from nonebot.typing import T_State
+from nonebot.log import logger
 
-translate = on_command('translate')
+translate = on_command('trans')
 
 
 def random_string_generator(length: int):
@@ -21,20 +22,24 @@ async def translate_handler(bot: Bot, event: Event, state: T_State):
     args = str(event.get_message()).strip().split()
     rand_str = random_string_generator(30)
     hash_digest = hashlib.md5(
-        str(nonebot.get_driver().config.BD_TRANSAPP_ID).encode('utf-8') +
+        str(nonebot.get_driver().config.bd_transapp_id).encode('utf-8') +
         args[2].encode('utf-8') +
         rand_str.encode('utf-8') +
-        str(nonebot.get_driver().config.BD_TRANSAPP_SECRET).encode('utf-8')
+        str(nonebot.get_driver().config.bd_transapp_secret).encode('utf-8')
     ).hexdigest()
     async with httpx.AsyncClient() as client:
         try:
             await translate.send("稍等下，正在获取您请求的信息……")
+            logger.debug("Requesting: https://fanyi-api.baidu.com/api/trans/vip/translate?q=" + args[2] +
+                "&from=" + args[0] + "&to=" + args[1] + "&appid=" + str(nonebot.get_driver().config.bd_transapp_id) +
+                "&salt=" + rand_str + "&sign=" + hash_digest)
             resp = await client.get(
                 "https://fanyi-api.baidu.com/api/trans/vip/translate?q=" + args[2] +
-                "&from=" + args[0] + "&to=" + args[1] + "&appid=" + str(nonebot.get_driver().config.BD_TRANSAPP_ID) +
+                "&from=" + args[0] + "&to=" + args[1] + "&appid=" + str(nonebot.get_driver().config.bd_transapp_id) +
                 "&salt=" + rand_str + "&sign=" + hash_digest, timeout=10.0)
         except httpx.ReadTimeout:
             await translate.finish("太对不起了，请求超时了（悲")
+        logger.debug(resp.json())
         result = resp.json()['trans_result'][0]['dst']
         await translate.finish(
             Message(
